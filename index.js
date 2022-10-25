f0abe962f8b345274e2b23422b93587099ba10fa
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-10-25 21:46:45 +0600

    api called correctly,this time,added node-modules to .gitignore

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/courses", (req, res) => {
 
 app.get("/courses/:id", (req, res) => {
   const id = req.params.id;
-  const selectedCourse = courses.find((c) => c.id === id);
+  const selectedCourse = courses.find((course) => course.id === id);
   res.send(selectedCourse);
 });
 
